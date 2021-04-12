17436414df1bbea3b8063ba4a6016459224b7400
Alexsandre Akofovi <52098@etu.he2b.be>
2021-04-12 16:20:09 +0200

    projet 3 : 2.1 | Les composants : fait

---
+++ javascript/model/Component.js
@@ -0,0 +1,18 @@
+/**
+ * A component 
+ */
+class Component {
+
+    /**
+     * 
+     * @param {string} id unique Component
+     */
+    constructor(id) {
+        this._id = id;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get id() { return this_id; }
+}
--- javascript/model/Tile.js
+++ javascript/model/Tile.js
@@ -1,18 +1,13 @@
 /**
  * A tile is a fixed Pacman maze component like a wall or a dot.
  */
-class Tile {
+class Tile extends Component{
 
     /**
      * To be created, a Tile just need an id
      * @param {string} id unique tile’s id 
      */
     constructor(id) {
-        this._id = id;
+        super(id);
     }
-
-    /**
-     * @returns {string}
-     */
-    get id() { return this_id; }
 }
